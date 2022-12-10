3acc38fd2f0bb8378e08ac8c137eec5b12fda60a
prashantkchouhan <pkchouhan490@gmail.com>
2022-12-10 18:46:39 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages,setup
+
+
+def get_requirements():
+    pass
+
+
+setup(
+    name="setup",
+    version="0.0.1",
+    author="ineuron",
+    author_email="pkchouhan490@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements(),
+)
